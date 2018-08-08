30a32059e37e87a804a2aec751977564f7d54946
xm <dxp157@sohu.com>
2018-08-08 22:48:41 +0800

    main.js: fix the bug of can't show the last image

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-$('#images > img:nth-child(1)').addClass('show');
-$('#images > img:nth-child(2)').addClass('arrival');
-$('#images > img:nth-child(3)').addClass('arrival');
-
 var index = 0;
 index += 1;
 
+var num = $('#images > img').length;
+for (let i=0; i<num; i++) {
+  if (i === 0) {
+    $(`#images > img:nth-child(${i+1})`).addClass('show');
+  }
 
-function limitIndex(index) {
-    // if index = 6, 9, 12...
-    if (index > 3) {
-      index = index % 3;
-      if (index === 0) {
-        index = 3;
-      }
-    }
-    return index;
+  $(`#images > img:nth-child(${i+1})`).addClass('arrival');
 }
 
-function moveImage(index, delayTime) {
+function limitIndex(imgIndex, imgNum) {
+    if (imgIndex > imgNum) {
+      // if index is multiple of imgNum like imgNum * n
+      imgIndex = imgIndex % imgNum;
+      if (imgIndex === 0) {
+        imgIndex = imgNum;
+      }
+    }
+    return imgIndex;
+}
+
+function moveImage(delayTime, imgNum) {
   setInterval(()=>{
-      let $img = $(`#images > img:nth-child(${limitIndex(index)})`);
+      let $img = $(`#images > img:nth-child(${limitIndex(index, imgNum)})`);
       $img.removeClass('show').addClass('departure')
           .one('transitionend', (e)=>{
               // when transition event is end
               $(e.currentTarget).removeClass('departure').addClass('arrival');
           });
       index += 1;
-      $img = $(`#images > img:nth-child(${limitIndex(index)})`);
+      $img = $(`#images > img:nth-child(${limitIndex(index, imgNum)})`);
       $img.removeClass('arrival').addClass('show');
   }, delayTime);
 }
 
-moveImage(index, 2000);
+moveImage(2000, num);
