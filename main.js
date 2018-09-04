98c41ec2fcd8d10443d62a56e79b186179c39a60
xm <dxp157@sohu.com>
2018-09-04 14:37:12 +0800

    main.js: Optimize the code of gotoSlide

--- main.js
+++ main.js
@@ -7,6 +7,34 @@ let $imagesChild = $images.children('img');
 let $firstFake = $imagesChild.eq(0).clone(true);
 let $lastFake = $imagesChild.eq($imagesChild.length - 1).clone(true);
 
+function gotoSlide(buttonIndex, curIndex) {
+  if (buttonIndex === 0 && curIndex === $buttons.length - 1) {
+    // 从最后一张跳转到第一张
+    $('#images').css({
+      transform: `translateX(${-(curIndex+2)*400}px)`
+    }).one('transitionend', function() {
+      $('#images').hide().offset();
+      $('#images').css({
+        transform: `translateX(${-(buttonIndex+1)*400}px)`
+      }).show();
+    })
+  } else if (buttonIndex === $buttons.length - 1 && curIndex === 0) {
+    // 从第一张跳转到最后一张
+    $('#images').css({
+      transform: 'translateX(0px)'
+    }).one('transitionend', function() {
+      $('#images').hide().offset();
+      $('#images').css({
+        transform: `translateX(${-(buttonIndex+1)*400}px)`
+      }).show();
+    })
+  } else {
+    $('#images').css({
+      transform: `translateX(${-(buttonIndex+1)*400}px)`
+    });
+  }
+}
+
 $images.prepend($lastFake);
 $images.append($firstFake);
 
@@ -16,55 +44,11 @@ $images.css({
 });
 $images.show();
 
-$buttons.eq(0).on('click', function() {
-  console.log('index = ' + index);
-  if (index === 4) {
-    console.log('从 最后一张 跳到 第一张');
-    $('#images').css({
-         transform: 'translateX(-2000px)'
-    }).one('transitionend', function() {
-      console.log('动画结束');
-      $('#images').hide().offset();
-      $('#images').css({
-        transform: 'translateX(-400px)'
-      }).show();
-    })
-  } else {
-    $('#images').css({
-      transform: 'translateX(-400px)'
-    });
-  }
-  index = 1;
-});
-$buttons.eq(1).on('click', function() {
-  $('#images').css({
-    transform: 'translateX(-800px)'
-  });
-  index = 2;
-});
-$buttons.eq(2).on('click', function() {
-  $('#images').css({
-    transform: 'translateX(-1200px)'
-  });
-  index = 3;
-});
-$buttons.eq(3).on('click', function() {
-  if (index === 1) {
-    console.log('从 第一张 跳到 最后一张');
-    $('#images').css({
-      transform: 'translateX(0px)'
-    }).one('transitionend', function() {
-      console.log('动画结束');
-      $('#images').hide().offset();
-      $('#images').css({
-        transform: 'translateX(-1600px)'
-      }).show();
 
-    })
-  } else {
-    $('#images').css({
-      transform: 'translateX(-1600px)'
-    });
-  }
-  index = 4;
+let curIndex = 0;
+$('#buttonWrapper').on('click', 'button', function(e) {
+  let $curButton = $(e.currentTarget);
+  let buttonIndex = $curButton.index();
+  gotoSlide(buttonIndex, curIndex);
+  curIndex = buttonIndex;
 });
